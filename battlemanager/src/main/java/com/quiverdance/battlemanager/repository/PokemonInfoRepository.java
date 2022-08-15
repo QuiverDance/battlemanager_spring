1ced77849d4a33c46f4a2ed064df15eb9efacff6
여름 <pjw7200@ajou.ac.kr>
2022-08-15 20:12:38 +0900

    save 기능 추가

--- battlemanager/src/main/java/com/quiverdance/battlemanager/repository/PokemonInfoRepository.java
+++ battlemanager/src/main/java/com/quiverdance/battlemanager/repository/PokemonInfoRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PokemonInfoRepository {
+    PokemonInfo save(PokemonInfo pokemonInfo);
     Optional<PokemonInfo> findById(Long id);
     Optional<PokemonInfo> findByName(String name);
     List<PokemonInfo> findAll();
