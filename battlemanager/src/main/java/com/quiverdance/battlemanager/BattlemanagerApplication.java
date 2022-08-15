6fb937f8936f6e081bfcf00c031da3d54cc6c2cc
여름 <pjw7200@ajou.ac.kr>
2022-08-15 20:06:11 +0900

    Initial commit

---
+++ battlemanager/src/main/java/com/quiverdance/battlemanager/BattlemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.quiverdance.battlemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlemanagerApplication.class, args);
+	}
+
+}
