888b148174abfc69a06d19816ee5377783d34960
여름 <pjw7200@ajou.ac.kr>
2022-08-18 18:05:16 +0900

    moveInfo 관련 의존성주입

--- battlemanager/src/main/java/com/quiverdance/battlemanager/SpringConfig.java
+++ battlemanager/src/main/java/com/quiverdance/battlemanager/SpringConfig.java
@@ -1,28 +1,29 @@
 package com.quiverdance.battlemanager;
 
-import com.quiverdance.battlemanager.repository.MemoryPokemonInfoRepository;
-import com.quiverdance.battlemanager.repository.PokemonInfoRepository;
-import com.quiverdance.battlemanager.service.PokemonInfoService;
+import com.quiverdance.battlemanager.repository.JdbcTemplateMoveInfoRepository;
+import com.quiverdance.battlemanager.repository.MoveInfoRepository;
+import com.quiverdance.battlemanager.service.MoveInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
 
-    private final PokemonInfoRepository pokemonInfoRepository;
     @Autowired
-    public SpringConfig(PokemonInfoRepository pokemonInfoRepository) {
-        this.pokemonInfoRepository = pokemonInfoRepository;
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
     }
 
     @Bean
-    public PokemonInfoService pokemonInfoService(){
-        return new PokemonInfoService(pokemonInfoRepository);
+    public MoveInfoService moveInfoService(){
+        return new MoveInfoService(moveInfoRepository());
     }
-
     @Bean
-    public PokemonInfoRepository pokemonInfoRepository(){
-        return new MemoryPokemonInfoRepository();
+    public MoveInfoRepository moveInfoRepository(){
+        return new JdbcTemplateMoveInfoRepository(dataSource);
     }
 }
