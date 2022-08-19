d8094c952af88841dd2944306de87693e095c476
여름 <pjw7200@ajou.ac.kr>
2022-08-19 19:30:10 +0900

    pokemonInfo controller 기능 추가

--- battlemanager/src/main/java/com/quiverdance/battlemanager/controller/PokemonInfoController.java
+++ battlemanager/src/main/java/com/quiverdance/battlemanager/controller/PokemonInfoController.java
@@ -1,17 +1,45 @@
 package com.quiverdance.battlemanager.controller;
 
+import com.quiverdance.battlemanager.domain.PokemonInfo;
 import com.quiverdance.battlemanager.service.PokemonInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PokemonInfoController {
-//    private final PokemonInfoService pokemonInfoService;
-//
-//    @Autowired
-//    public PokemonInfoController(PokemonInfoService pokemonInfoService) {
-//        this.pokemonInfoService = pokemonInfoService;
-//    }
+    private final PokemonInfoService pokemonInfoService;
 
+    @Autowired
+    public PokemonInfoController(PokemonInfoService pokemonInfoService) {
+        this.pokemonInfoService = pokemonInfoService;
+    }
+
+    @GetMapping("/pokemon/all")
+    public List<PokemonInfo> getPokemonList(){
+        return pokemonInfoService.findAllPokemonInfo();
+    }
+
+    @GetMapping("/pokemon/all")
+    public List<String> getPokemonNameList(){
+        List<PokemonInfo> pokemonInfoList = pokemonInfoService.findAllPokemonInfo();
+        List<String> pokemonNameList = List.of();
+        int size = pokemonInfoList.size();
+        
+        for(int i = 0; i < size; i++){
+            pokemonNameList.add(pokemonInfoList.get(i).getDexId() + ". " + pokemonInfoList.get(i).getName());
+        }
+
+        return pokemonNameList;
+    }
+
+    @GetMapping("/pokemon/one")
+    public Optional<PokemonInfo> getPokemon(@RequestParam String name){
+        return pokemonInfoService.findPokemonInfo(name);
+    }
 
 }
