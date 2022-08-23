a8375ad623e0e76dd607e76f8192bdc3d2ae641b
여름 <pjw7200@ajou.ac.kr>
2022-08-23 22:20:33 +0900

    PokemonInfoController test 코드

---
+++ battlemanager/src/test/java/com/quiverdance/battlemanager/controller/PokemonInfoControllerTest.java
@@ -0,0 +1,26 @@
+package com.quiverdance.battlemanager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class PokemonInfoControllerTest {
+    @Autowired
+    PokemonInfoController pokemonInfoController;
+
+    @Test
+    void getAllPokemonInfo(){
+        ResponseEntity<?> result = pokemonInfoController.getPokemonList();
+        System.out.println(result.toString());
+    }
+
+    @Test
+    void getPokemonNames(){
+        ResponseEntity<?> result = pokemonInfoController.getPokemonNameList();
+        System.out.println(result.toString());
+    }
+}
