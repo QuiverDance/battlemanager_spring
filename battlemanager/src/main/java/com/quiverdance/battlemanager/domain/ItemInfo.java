72a81116ac77063d6a69ecd2ccc4a075575ec62d
QuiverDance <pjw7200@ajou.ac.kr>
2022-09-01 21:03:47 +0900

    category 추가

--- battlemanager/src/main/java/com/quiverdance/battlemanager/domain/ItemInfo.java
+++ battlemanager/src/main/java/com/quiverdance/battlemanager/domain/ItemInfo.java
@@ -3,5 +3,29 @@ package com.quiverdance.battlemanager.domain;
 public class ItemInfo {
     private Long id;
     private String name;
-    private String description;
+    private String category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
